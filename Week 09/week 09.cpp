67eb4dba3ef9cc1cad4e516cf686f76bac4e553d
farhanarahaman976 <farhana.rahaman37@gmail.com>
2025-02-19 22:31:06 +0600

    Update week 09.cpp

--- Week 09/week 09.cpp
+++ Week 09/week 09.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <vector>
 #include <bitset>
@@ -9,14 +8,14 @@ using namespace std;
 
 class CPU {
 private:
-    vector<int> registers;
+    vector<bitset<8>> registers;
     map<int, vector<string>> memory;
     int pc;
     bool running;
     ofstream logFile;
 
 public:
-    CPU() : registers(8, 0), pc(0), running(true) {
+    CPU() : registers(8), pc(0), running(true) {
         logFile.open("cpu_log.txt");
     }
 
@@ -46,57 +45,68 @@ public:
 
     void processInstruction(const vector<string>& instr) {
         string opcode = instr[0];
+
         if (opcode == "INPUT") {
             int reg = stoi(instr[1]);
-            int value;
-            cout << "Enter value: ";
-            cin >> value;
-            registers[reg] = value;
+            string binary_value;
+            cout << "Enter binary value (8-bit): ";
+            cin >> binary_value;
+            registers[reg] = bitset<8>(binary_value); 
             displayBinary("INPUT", reg);
-        } else if (opcode == "OUTPUT") {
+        }
+        else if (opcode == "OUTPUT") {
             int reg = stoi(instr[1]);
-            cout << "OUTPUT R" << reg << " = " << registers[reg] << " (" << bitset<8>(registers[reg]) << ")\n";
-            logFile << "OUTPUT R" << reg << " = " << registers[reg] << " (" << bitset<8>(registers[reg]) << ")\n";
-        } else if (opcode == "ADD") {
+            cout << "OUTPUT R" << reg << " = " << registers[reg] << endl;
+            logFile << "OUTPUT R" << reg << " = " << registers[reg] << endl;
+        }
+        else if (opcode == "ADD") {
             int r1 = stoi(instr[1]), r2 = stoi(instr[2]), r3 = stoi(instr[3]);
-            registers[r1] = registers[r2] + registers[r3];
+            registers[r1] = bitset<8>(registers[r2].to_ulong() + registers[r3].to_ulong()); 
             displayBinary("ADD", r1);
-        } else if (opcode == "SUB") {
+        }
+        else if (opcode == "SUB") {
             int r1 = stoi(instr[1]), r2 = stoi(instr[2]), r3 = stoi(instr[3]);
-            registers[r1] = registers[r2] - registers[r3];
+            registers[r1] = bitset<8>(registers[r2].to_ulong() - registers[r3].to_ulong()); 
             displayBinary("SUB", r1);
-        } else if (opcode == "MUL") {
+        }
+        else if (opcode == "MUL") {
             int r1 = stoi(instr[1]), r2 = stoi(instr[2]), r3 = stoi(instr[3]);
-            registers[r1] = registers[r2] * registers[r3];
+            registers[r1] = bitset<8>(registers[r2].to_ulong() * registers[r3].to_ulong()); 
             displayBinary("MUL", r1);
-        } else if (opcode == "JUMP") {
+        }
+        else if (opcode == "JUMP") {
             int address = stoi(instr[1]);
             pc = address;
-        } else if (opcode == "BEQ") {
+        }
+        else if (opcode == "BEQ") {
             int r1 = stoi(instr[1]), r2 = stoi(instr[2]), address = stoi(instr[3]);
             if (registers[r1] == registers[r2]) {
                 pc = address;
             }
-        } else if (opcode == "LOAD") {
+        }
+        else if (opcode == "LOAD") {
             int r1 = stoi(instr[1]), address = stoi(instr[2]);
-            registers[r1] = stoi(memory[address][0]);
+            registers[r1] = bitset<8>(memory[address][0]); 
             displayBinary("LOAD", r1);
-        } else if (opcode == "STORE") {
+        }
+        else if (opcode == "STORE") {
             int r1 = stoi(instr[1]), address = stoi(instr[2]);
-            memory[address] = {to_string(registers[r1])};
-        } else if (opcode == "HALT") {
+            memory[address] = {registers[r1].to_string()};
+        }
+        else if (opcode == "HALT") {
             running = false;
             cout << "CPU HALTED\n";
             logFile << "CPU HALTED\n";
-        } else {
+        }
+        else {
             cout << "Unknown instruction: " << opcode << endl;
             logFile << "Unknown instruction: " << opcode << endl;
         }
     }
 
     void displayBinary(string op, int reg) {
-        cout << op << " R" << reg << " = " << registers[reg] << " (" << bitset<8>(registers[reg]) << ")\n";
-        logFile << op << " R" << reg << " = " << registers[reg] << " (" << bitset<8>(registers[reg]) << ")\n";
+        cout << op << " R" << reg << " = " << registers[reg] << endl;
+        logFile << op << " R" << reg << " = " << registers[reg] << endl;
     }
 };
 
